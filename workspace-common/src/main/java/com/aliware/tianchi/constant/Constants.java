41df23c04bf54e9707c6ef6be3c1d32e644442b7
tytao <1522014784@qq.com>
2021-10-27 17:25:35 +0800

    update pram

--- workspace-common/src/main/java/com/aliware/tianchi/constant/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.aliware.tianchi.constant;
-
-public interface Constants {
-    String SERVER_START_TIME_KEY = "server_start_time_key";
-    String CLIENT_START_TIME_KEY = "client_start_time_key";
-
-    String CONCURRENT_ALLOW_KEY = "concurrent_allow_key";
-}
