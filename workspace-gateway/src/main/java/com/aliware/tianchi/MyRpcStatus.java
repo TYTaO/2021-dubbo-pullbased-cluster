8d1dd6e801d7d9b10ac34fbe55e2fd8e7a81cb14
tytao <1522014784@qq.com>
2021-10-27 18:49:42 +0800

    add MyRpcStatus

---
+++ workspace-gateway/src/main/java/com/aliware/tianchi/MyRpcStatus.java
@@ -0,0 +1,52 @@
+package com.aliware.tianchi;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.rpc.RpcStatus;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MyRpcStatus {
+    private static final ConcurrentMap<String, MyRpcStatus> MY_SERVICE_STATISTICS = new ConcurrentHashMap<String,
+            MyRpcStatus>();
+
+    private static final int initTimeout = 160;
+    public final AtomicInteger LastElapsed = new AtomicInteger();
+//    public static AtomicLong initCount = new AtomicLong(1000);
+
+    private MyRpcStatus() {
+    }
+
+    public static MyRpcStatus getStatus(URL url) {
+        String uri = url.toIdentityString();
+        return MY_SERVICE_STATISTICS.computeIfAbsent(uri, key -> new MyRpcStatus());
+    }
+
+    public static void endCount(URL url, long elapsed, boolean succeeded) {
+        MyRpcStatus status = getStatus(url);
+        if (succeeded) {
+            status.LastElapsed.set((int) elapsed);
+        } else {
+            if (elapsed > status.LastElapsed.get()) {
+                status.LastElapsed.set((int) elapsed);
+            }
+        }
+    }
+
+    public static Integer getTimeout(URL url) {
+        MyRpcStatus status = getStatus(url);
+        int lastElapsed = status.LastElapsed.get();
+        if (lastElapsed == 0) {
+            return initTimeout;
+        }
+        int max = (int) RpcStatus.getStatus(url).getSucceededAverageElapsed() * 3;
+        if (lastElapsed > max) {
+            return max;
+        }
+        return lastElapsed + 3; // magic num
+    }
+}
