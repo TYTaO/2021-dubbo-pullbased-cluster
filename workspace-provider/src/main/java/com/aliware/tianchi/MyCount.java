4e6da82dd8aedee746cb704fb6ef689dfbd69918
tytao <1522014784@qq.com>
2021-10-28 09:10:54 +0800

    no preheat

--- workspace-provider/src/main/java/com/aliware/tianchi/MyCount.java
+++ workspace-provider/src/main/java/com/aliware/tianchi/MyCount.java
@@ -12,14 +12,14 @@ public class MyCount {
     private static final ConcurrentMap<String, MyCount> SERVICE_STATISTICS = new ConcurrentHashMap<String,
             MyCount>();
 
-    private static final int initMax = 30;
+    private static final int initMax = 50;
     private static final int initStep = 3;
     private static final int maxStepAbs = 3;
     private static final int minStepAbs = 3;
     private static final long interval = 2500; // 2s
     private static final long preheatInterval = 600; // 2s
-    private static final int stepAbsFix = 4; //
-    private static final long preheatIntervalSum = 50000; //
+    private static final int stepAbsFix = 3; //
+    private static final long preheatIntervalSum = 5000000; //50000
     private static final long preheatSumDdl = System.currentTimeMillis() + preheatIntervalSum; //
 
 
